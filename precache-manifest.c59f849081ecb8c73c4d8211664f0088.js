038229020c24a49133763749b4505b40f67f8dcd
tawiii <4etkiu94@mail.ru>
2018-11-07 17:22:17 +0200

    Updates

---
+++ precache-manifest.c59f849081ecb8c73c4d8211664f0088.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dc72d8dfcb21b3611af",
+    "url": "/codempire/static/js/runtime~main.7dc72d8d.js"
+  },
+  {
+    "revision": "e347fd2878363103f834",
+    "url": "/codempire/static/js/main.e347fd28.chunk.js"
+  },
+  {
+    "revision": "a48c7e2a1b567e342c8b",
+    "url": "/codempire/static/js/1.a48c7e2a.chunk.js"
+  },
+  {
+    "revision": "e347fd2878363103f834",
+    "url": "/codempire/static/css/main.1f56f896.chunk.css"
+  },
+  {
+    "revision": "6433e6f594b8c29f1e2e9c99db412269",
+    "url": "/codempire/index.html"
+  }
+];
